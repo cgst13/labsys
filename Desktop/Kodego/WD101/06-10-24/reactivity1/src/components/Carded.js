c5dab4d4a5d4c72799160cbf00babeeab36e383c
cgst13 <bsit.christiantolentino@gmail.com>
2024-06-11 18:16:51 +0800

    V1

---
+++ Desktop/Kodego/WD101/06-10-24/reactivity1/src/components/Carded.js
@@ -0,0 +1,33 @@
+function Carded() {
+    return (
+      <>
+          <div class="row">
+        <div class="col-sm-6 mb-3 mb-sm-0">
+          <div class="card">
+            <img src="images/amsterdam.jpg" class="card-img-top" alt="..."></img>
+            <div class="card-body">
+              <h5 class="card-title">Special title treatment</h5>
+              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-6 mb-3 mb-sm-0">
+            <div class="card">
+            <img src="images/amsterdam.jpg" class="card-img-top" alt="..."></img>
+            <div class="card-body">
+              <h5 class="card-title">Special title treatment</h5>
+              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      
+      </>
+
+
+    );
+  }
+  
+  export default Carded;
---
+++ Desktop/Kodego/WD101/06-10-24/reactivity1/src/components/Template.js
@@ -0,0 +1,11 @@
+function Template() {
+    return (
+      <div>
+           
+
+
+      </div>
+    );
+  }
+  
+  export default Template;
